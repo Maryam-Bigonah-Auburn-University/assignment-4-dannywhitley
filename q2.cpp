80d8da52b3e1e394e2be4b5f8ce811782d128c50
dannywhitley <dzw0064@auburn.edu>
2024-10-28 13:57:14 -0500

    Update q2.cpp

--- q2.cpp
+++ q2.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <cstdlib>
 using namespace std;
 
 class Temperature {
@@ -7,15 +7,73 @@ class Temperature {
 		double degreesKelvin,
 		       degreesFahrenheit,
 		       degreesCelsius;
+
 	public:
-		void setTempKelvin(double degreesKelvin) {  }
-		double getTempKelvin() {  }
-		void setTempCelsius() {  }
-		double getTempCelsius() {  } 
-		void setTempFahrenheit() {  }
-		double getTempFahrenheit() { }
+		// Kelvin mutator and accessor functions
+		void setTempKelvin(double kelvin);
+		double getTempKelvin();
+
+		// Celsius mutator and accessor functions
+		void setTempCelsius();
+		double getTempCelsius();
+
+		// Fahrenheit mutator and accessor function
+		void setTempFahrenheit();
+		double getTempFahrenheit();
 };
 
 int main() {
+	Temperature temp1;
+	double myTemp;
 
+	// Prompts user to enter a value
+	cout << "\n";
+	cout << "Enter a temperature in Kelvin: "; 
+	cin >> myTemp;
+	cout << endl;
+
+	// Will exit the code if an invalid temperature is entered
+	if (myTemp < 0) {
+		cout << "Invalid temperature entered.";
+		exit(1);
+	}
+
+	// Function calls to convert and set the temperatures
+	temp1.setTempKelvin(myTemp);
+	temp1.setTempCelsius();
+	temp1.setTempFahrenheit();
+
+	cout << "Temperature Conversions:" << endl;
+	cout << "Kelvin: " << temp1.getTempKelvin() << "K"<< endl;
+	cout << "Celsius: " << temp1.getTempCelsius() << "C" << endl;
+	cout << "Fahrenheit: " << temp1.getTempFahrenheit() << "F" << endl;
+	cout << "\n";
+
+    return 0;
+}
+
+// Accessor functions
+double Temperature::getTempKelvin() {
+	return degreesKelvin;
+}
+
+double Temperature::getTempCelsius() {
+	return degreesCelsius;
+}
+
+double Temperature::getTempFahrenheit() {
+	return degreesFahrenheit;
+}
+
+// Mutator functions
+void Temperature::setTempKelvin(double kelvin) {
+	degreesKelvin = kelvin;
+}
+
+void Temperature::setTempCelsius() {
+	degreesCelsius = degreesKelvin - 273.15;
+}
+
+void Temperature::setTempFahrenheit() {
+	degreesFahrenheit = (9.0 / 5) * degreesCelsius + 32;
 }
